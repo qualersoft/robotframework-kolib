6bb43abb0951da440d414b0176ea017fdfc2ea84
Mathze <270275+mathze@users.noreply.github.com>
2021-06-20 20:46:49 +0200

    #14 implemented source functionality in kolib-spring RobotLib;

--- robotframework-kolib-spring/src/main/kotlin/de/qualersoft/robotframework/library/RobotLib.kt
+++ robotframework-kolib-spring/src/main/kotlin/de/qualersoft/robotframework/library/RobotLib.kt
@@ -2,22 +2,43 @@ package de.qualersoft.robotframework.library
 
 import de.qualersoft.robotframework.library.annotation.Keyword
 import de.qualersoft.robotframework.library.binding.MinimalDynamicLibrary
-import de.qualersoft.robotframework.library.binding.RFArgumentSpecSupport
-import de.qualersoft.robotframework.library.binding.RFArgumentTypesSupport
-import de.qualersoft.robotframework.library.binding.RFKwArgsSupport
+import de.qualersoft.robotframework.library.binding.RfArgumentSpecSupport
+import de.qualersoft.robotframework.library.binding.RfArgumentTypesSupport
+import de.qualersoft.robotframework.library.binding.RfKwArgsSupport
 import de.qualersoft.robotframework.library.binding.RfLibdocSupport
+import de.qualersoft.robotframework.library.binding.RfSourceSupport
 import de.qualersoft.robotframework.library.model.KeywordDescriptor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.ConfigurableApplicationContext
+import java.io.File
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 import kotlin.jvm.internal.Reflection
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KVisibility
 import kotlin.reflect.full.memberFunctions
 
-open class RobotLib(root: KClass<*>, vararg args: String) : MinimalDynamicLibrary, RFKwArgsSupport,
-  RFArgumentSpecSupport, RFArgumentTypesSupport, RfLibdocSupport {
+/**
+ * Simply extend a class by this library and provide a entry-point class (root) from which to start scanning.
+ *
+ * **Remarks:**
+ *
+ * The robot framework documentation stats to create a Library-class at `empty` package level to avoid
+ * long paths when using in robot-files.
+ *
+ * If you do so, you can simply create a marker interface/class in your sub package.
+ * 
+ * **Attention:**
+ * 
+ * Do not force springs class scanning to operate on the `empty` package! This may either 
+ * lead to out of memory exception or take a large initialization time.
+ * 
+ * Try to put [root] as close as possible to your implementation.
+ */
+open class RobotLib(private val root: KClass<*>, vararg args: String) : MinimalDynamicLibrary, RfKwArgsSupport,
+  RfArgumentSpecSupport, RfArgumentTypesSupport, RfLibdocSupport, RfSourceSupport {
 
   private val log: Logger = LoggerFactory.getLogger(javaClass)
   private val ctx: ConfigurableApplicationContext
@@ -26,7 +47,6 @@ open class RobotLib(root: KClass<*>, vararg args: String) : MinimalDynamicLibrar
 
   private val keyWords: Map<String, KeywordDescriptor> by lazy { findKeywordFunctions() }
 
-
   init {
     log.debug("Initializing ${this::class.simpleName} with ${root.simpleName}")
     @SuppressWarnings("SpreadOperator")
@@ -34,8 +54,11 @@ open class RobotLib(root: KClass<*>, vararg args: String) : MinimalDynamicLibrar
     log.debug("${this::class} initialized ${root.simpleName}")
   }
 
+  /**
+   * Constructor that easily accepts java-classes with out the need of the user to deal with reflection stuff.
+   */
   @SuppressWarnings("SpreadOperator")
-  constructor(root: Class<*>, vararg args: String): this(Reflection.createKotlinClass(root), *args)
+  constructor(root: Class<*>, vararg args: String) : this(Reflection.createKotlinClass(root), *args)
 
   final override fun getKeywordNames(): List<String> {
     return keyWords.keys.toList()
@@ -78,6 +101,30 @@ open class RobotLib(root: KClass<*>, vararg args: String) : MinimalDynamicLibrar
     "__init__" -> getLibraryUsageDocumentation()
     else -> keyWords.getValue(name).description
   }
+
+  override fun getKeywordSource(name: String): String? {
+    val kwd = keyWords.getValue(name)
+    val pathToJar = rootPath ?: return null
+    val classPath = kwd.declaringClass.qualifiedName?.replace('.', File.separatorChar)
+    return "$pathToJar${File.separatorChar}$classPath" 
+  }
+
+  private val rootPath by lazy {
+    val src = root.java.protectionDomain.codeSource
+    return@lazy if (null != src) {
+      File(src.location.toURI())
+    } else {
+      val path = root.java.getResource("${root.java.simpleName}.class")?.path?.let {
+        it.substring(it.indexOf(':')+1, it.indexOf('!'))
+      }
+      if (null == path) {
+        null
+      } else {
+        val jarFilePath = URLDecoder.decode(path, StandardCharsets.UTF_8)
+        File(jarFilePath)
+      }
+    }
+  } 
 
   /**
    * Meant to be overwritten. Default impl returns empty string
