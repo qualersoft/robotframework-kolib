2c2233e28f73b4e3270e26a1996c5e7bb98563a9
Mathze <270275+mathze@users.noreply.github.com>
2022-05-15 13:54:36 +0200

    #32 Fix snapshot release & bump versions;

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,14 +10,14 @@ pluginManagement {
     kotlin("plugin.spring") version kotlinVersion
 
     // spring
-    id("org.springframework.boot") version "2.4.5"
+    id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
 
     // quality
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
+    id("io.gitlab.arturbosch.detekt") version "1.20.0"
 
     // documentation
-    id("org.jetbrains.dokka") version "1.4.32"
+    id("org.jetbrains.dokka") version "1.6.21"
     id("org.asciidoctor.jvm.convert") version "3.3.2"
   }
 }
